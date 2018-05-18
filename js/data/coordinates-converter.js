e958e6d88ca1012b19c25e8634b2614ddb854cd1
Dmitry Spiridonov <sydd2005@gmail.com>
2018-05-18 17:17:30 +0300

    улучшает геокодирование улиц

--- js/data/coordinates-converter.js
+++ js/data/coordinates-converter.js
@@ -3,6 +3,8 @@ import config from "../config";
 
 const CITY_TYPE = `locality`;
 const STREET_TYPE = `route`;
+const GEOCODE_LANGUAGE = `ru`;
+const GEOCODE_FILTER = `street_address|locality`;
 
 const findLongName = (addressComponents, desiredType) => {
   let searchResult = addressComponents.find((component) => component.types.includes(desiredType));
@@ -12,7 +14,7 @@ const findLongName = (addressComponents, desiredType) => {
 const CoordinatesConverter = class {
 
   static async toShortAddress(lat, lng) {
-    const geocodeUrl = `${config.GEOCODE_URL_BASE}?latlng=${lat},${lng}&key=${config.GEOCODE_KEY}&language=ru`;
+    const geocodeUrl = `${config.GEOCODE_URL_BASE}?latlng=${lat},${lng}&key=${config.GEOCODE_KEY}&language=${GEOCODE_LANGUAGE}&result_type=${GEOCODE_FILTER}`;
     const geocodeResponse = await DataLoader.loadJson(geocodeUrl);
     const cityName = findLongName(geocodeResponse.results[0].address_components, CITY_TYPE);
     const streetName = findLongName(geocodeResponse.results[0].address_components, STREET_TYPE);
